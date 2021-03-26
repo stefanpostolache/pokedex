3572fdc48caf59e20a8341bdada734b0b1aefd4c
Stefan Postolache <stefan.freya@gmail.com>
2021-03-26 10:53:52 +0000

    added pokemon image

--- src/components/pokemon-image.js
+++ src/components/pokemon-image.js
@@ -1,8 +1,30 @@
+import { useSelector } from "react-redux";
+import { bigImageUrlForPokemonWithId } from "../api";
+import styled from 'styled-components';
+
 
 export default function PokemonImage () {
+
+    const selectedPokemon = useSelector(state => state.details.details.details);
+
     return (
-        <div className="pokemonImage">
-        </div>
+        <StyledPokemonImage>
+            {
+                selectedPokemon && <img src={bigImageUrlForPokemonWithId(selectedPokemon.id)} alt={selectedPokemon.name} />
+            }
+        </StyledPokemonImage>
     )
 }
+
+const StyledPokemonImage = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+        transition: transform 0.5s ease-in-out;
+        &:hover {
+            transform: scale(1.2);
+        }
+    }
+`
 
