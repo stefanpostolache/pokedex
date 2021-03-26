03ce8cb77d5d72aba0d37e5fb3d12f82265229d3
Stefan Postolache <stefan.freya@gmail.com>
2021-03-26 10:12:53 +0000

    finished styling items in pokemon list and added chevron animation to selected

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { imageUrlForPokemonWithId } from "../api";
+import chevronIcon from '../img/pokedex_chevron.png'
 
 export default function Pokemon ({pokemon}) {
     const urlParts = pokemon.url.split('/');
@@ -8,13 +9,28 @@ export default function Pokemon ({pokemon}) {
 
     return (
         <StyledPokemon className={pokemonId == 1 ? "selected" : ""}>
-            <img src={imageUrlForPokemonWithId(pokemonId)} alt={pokemon.name} />
+            <img className="pokemon-icon" src={imageUrlForPokemonWithId(pokemonId)} alt={pokemon.name} />
+            <div className="trapezium-black"></div>
+            <img className="chevron" src={chevronIcon} />
             <div className="name-number-container">
-                <h1>{`No. ${pokemonIdFormatted}`}</h1>&nbsp;&nbsp;&nbsp;<h2>{pokemon.name}</h2>
+                <div>
+                    <h1>{`No. ${pokemonIdFormatted}`}</h1>
+                </div>
+                <div>
+                    <h2>{pokemon.name}</h2>
+                </div>
             </div>
         </StyledPokemon>
     )
 }
+
+const chevronAnimation = keyframes`
+    from {
+        transform: translateX(-130%) translateY(-50%);
+    } to {
+        transform: translateX(-110%) translateY(-50%);
+    }
+`
 
 const StyledPokemon = styled.div`
     margin-top: 2rem;
@@ -24,8 +40,29 @@ const StyledPokemon = styled.div`
     border-radius: 30pt;
     position: relative;
 
+    .trapezium-black {
+        position: absolute;
+        background-color: transparent;
+        right: 0;
+        top: 0;
+        width: 55%;
+        height: 100%;
+        border-top-right-radius: 30pt;
+        border-bottom-right-radius: 30pt;
+        clip-path: polygon(5% 0%, 100% 0, 100% 100%, 0% 100%);
+    }
+
     h1, h2 {
-        font-size: 2.5em;
+        font-size: 2em;
+        margin: 5pt 0;
+    }
+
+    h1 {
+        margin-left: 40pt;
+    }
+
+    h2 {
+        text-transform: capitalize;
     }
 
     &.selected {
@@ -35,16 +72,39 @@ const StyledPokemon = styled.div`
             color: white;
             flex: 1;
         }
+        .trapezium-black {
+            background-color: black;
+        }
+        .chevron {
+            display: block;;
+        }
     }
 
     .name-number-container {
         display: flex;
+
+        & > div {
+            flex: 1;
+            display: inline-block;
+            z-index: 3;
+        } 
         
     }
 
-    img {
+    img.pokemon-icon {
         position: absolute;
+        left: 0;
         top: 50%;
-        transform: translateY(-50%);
+        transform: translateY(-50%) translateX(-20%);
     }
+
+    img.chevron {
+        position: absolute;
+        height: 45pt;
+        top: 50%;
+        transform: translateX(-130%) translateY(-50%);
+        animation: ${chevronAnimation} 1s ease infinite alternate;
+        display: none;
+    }
+
 `
